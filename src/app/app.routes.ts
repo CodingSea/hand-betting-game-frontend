d1693983feef25aba4f7cd2023ebdbb7dfa7f423
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2026-03-13 00:20:24 +0300

    added components to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { App } from './app';
+import { GameBoard } from './game-board/game-board';
+import { GameOver } from './game-over/game-over';
+import { Leaderboard } from './leaderboard/leaderboard';
 
 export const routes: Routes = [
-    {path: "", component: App},
-    // {path: "/game"}
+    {path: "", component: GameBoard},
+    {path: "game-over", component: GameOver},
+    {path: "leaderboard", component: Leaderboard}
 ];
